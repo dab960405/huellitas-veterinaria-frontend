fd3ed92a341a5effe2564c881438b7630bf891b5
Diego <diegobravo.c96@gmail.com>
2026-02-23 21:51:43 -0500

    feat: frontend React - Home, Dueños y Mascotas completos con responsive

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import HomePage from './components/home/HomePage';
+import DuenosPage from './components/duenos/DuenosPage';
+import MascotasPage from './components/mascotas/MascotasPage';
+// import CitasPage from './components/citas/CitasPage'; // Lo creamos mañana
+
+/**
+ * Componente principal de la aplicación.
+ * Define las rutas del sistema usando React Router.
+ */
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="duenos" element={<DuenosPage />} />
+                    <Route path="mascotas" element={<MascotasPage />} />
+                    {/* <Route path="citas" element={<CitasPage />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
